c865a4d464ebec5d927c53875acdbab11f6b52a4
Muhammad Nabeel Amin <61421054+Nabeel70@users.noreply.github.com>
2021-07-25 06:47:46 +0500

    app.svlt update 0.0.2

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -9,7 +9,7 @@
   });
     export let alt = false;
     let date = new Date();
-    date.setDate(date.getDate() + 13);
+    date.setDate(date.getDate() + 12);
     date.setHours(date.getHours() + 7);
     date.setMinutes(date.getMinutes() + 56);
     date.setSeconds(date.getSeconds() + 15);
